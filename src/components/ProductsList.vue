ab58465737106e3bce8ab8bad76e7184e98fea75
gloryyy <majdwarrrak@gmail.com>
2021-01-26 15:25:04 +0100

    setup the frontend part

---
+++ src/components/ProductsList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="products.length > 0">
+    <ProductsListItem
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+    />
+  </div>
+  <p v-else>You havn't added anything to your Cart yet!</p>
+</template>
+
+<script>
+import ProductsListItem from "../components/ProductsListItem";
+
+export default {
+  name: "ProductsList",
+  props: ["products"],
+  components: {
+    ProductsListItem,
+  },
+};
+</script>
---
+++ src/views/ProductsPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="page-wrap">
+    <ProductsGrid :products="products" />
+  </div>
+</template>
+
+<script>
+import { products } from "../fake-data";
+import ProductsGrid from "../components/ProductsGrid";
+
+export default {
+  name: "ProductsPage",
+  components: {
+    ProductsGrid,
+  },
+  data() {
+    return {
+      products,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.grid-wrap {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-top: 16px;
+}
+</style>
